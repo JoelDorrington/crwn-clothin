0367952d0195b6543c581f3adcdb609a3314eb2b
Joel Dorrington <joel_dorrington@hotmail.com>
2020-04-12 13:19:39 -0400

    Use dynamic route links

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss'
-const MenuItem = ({title, imageUrl, size, history}) => (
-  <div className={`${size} menu-item`} onClick={() => history.push(`/${title}`)}>
+const MenuItem = ({title, imageUrl, linkUrl, size, history, match}) => (
+  <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
     <div className="background-image" style={{
       backgroundImage: `url(${imageUrl})`
     }}></div>
